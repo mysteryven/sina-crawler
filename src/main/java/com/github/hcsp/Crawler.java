b54af72fd139b960154e71d97c406968a8ec9b00
wenzhe <mysteryven@gmail.com>
2020-04-06 10:27:33 +0800

    抽取公共方法，重构代码

--- src/main/java/com/github/hcsp/Crawler.java
+++ src/main/java/com/github/hcsp/Crawler.java
@@ -17,7 +17,7 @@ public class Crawler {
     public static void main(String[] args) throws IOException {
         HashMap<String, String> news = new HashMap<>();
 
-        Queue<String> linkToBeProcessed = new LinkedList();
+        Queue<String> linkToBeProcessed = new LinkedList<>();
 
         HashSet<String> linkHasBeenProcessed = new HashSet<>();
         linkToBeProcessed.add("https://sina.cn/");
@@ -26,35 +26,48 @@ public class Crawler {
 
         while(!linkToBeProcessed.isEmpty()) {
             String link = linkToBeProcessed.poll();
-            if (linkHasBeenProcessed.contains(link)) {
-                continue;
+
+            if (isNewPage(link, linkHasBeenProcessed)) {
+                Document document = getCurrentLinkDocument(httpclient, link);
+
+                storeProcessedLink(linkHasBeenProcessed, link);
+                storeNewsToDataBase(document, news);
+                storeALinkToLinkPool(linkToBeProcessed, document);
             }
-
-            linkHasBeenProcessed.add(link);
-
-            HttpGet httpGet = new HttpGet(link);
-            CloseableHttpResponse response = httpclient.execute(httpGet);
-            String responseString = EntityUtils.toString(response.getEntity());
-            Document document = Jsoup.parse(responseString);
-            Elements elements = document.select("a");
-
-            storeToDataBaseIfIsNewsPage(document);
-
-            for (Element element: elements) {
-                String href = element.attr("href");
-                if (href.contains("news.sina.cn")) {
-                    linkToBeProcessed.add(href);
-                }
-            }
-
         }
     }
 
-    private static void storeToDataBaseIfIsNewsPage(Document doc, HashMap news) {
+    private static void storeProcessedLink(HashSet<String> linkHasBeenProcessed, String link) {
+        linkHasBeenProcessed.add(link);
+    }
+
+    private static Document getCurrentLinkDocument(CloseableHttpClient httpclient, String link) throws IOException {
+        HttpGet httpGet = new HttpGet(link);
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        String responseString = EntityUtils.toString(response.getEntity());
+        return Jsoup.parse(responseString);
+    }
+
+    private static void storeALinkToLinkPool(Queue<String> linkToBeProcessed, Document document) {
+        Elements elements = document.select("a");
+        for (Element element: elements) {
+            String href = element.attr("href");
+            if (href.contains("news.sina.cn")) {
+                linkToBeProcessed.add(href);
+            }
+        }
+    }
+
+    private static boolean isNewPage(String link, HashSet<String> linkHasBeenProcessed) {
+        return !linkHasBeenProcessed.contains(link);
+    }
+
+    private static void storeNewsToDataBase(Document doc, HashMap<String, String> news) {
         Elements articles = doc.select("article");
         if (!articles.isEmpty()) {
             String title = articles.get(0).child(0).text();
             String text = articles.get(0).select("p.art_p").text();
+            System.out.println(title);
             news.put(title, text);
         }
     }
